285ceacc262152ac62589be2bfc0523aa0d1d045
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-02-01 18:17:56 +0100

    finished project

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 const mainBtn = document.querySelector('.sub');
-// const oneBtn = document.querySelector('.first');
-// const twoBtn = document.querySelector('.second');
-// const threeBtn = document.querySelector('.third');
-// const fourBtn = document.querySelector('.four');
-// const fiveBtn = document.querySelector('.five');
-
-const btns = document.querySelectorAll('.btn');
-
+const allBtnRank = document.querySelectorAll('.btn');
 const check = document.querySelector('.card__checkText');
 const error = document.querySelector('.card__errorText');
-
 const bodyOne = document.querySelector('.card__body');
 const bodyTwo = document.querySelector('.card__bodySecond');
+const buttonOne = allBtnRank[0];
+const buttonOneText = allBtnRank[0].textContent;
+const buttonTwo = allBtnRank[1];
+const buttonTwoText = allBtnRank[1].textContent;
+const buttonThree = allBtnRank[2];
+const buttonThreeText = allBtnRank[2].textContent;
+const buttonFour = allBtnRank[3];
+const buttonFourText = allBtnRank[3].textContent;
+const buttonFive = allBtnRank[4];
+const buttonFiveText = allBtnRank[4].textContent;
+const tabLong = allBtnRank.length;
+
 
 const reating = () => {
 	if (check.textContent == '') {
@@ -22,26 +26,69 @@ const reating = () => {
 	}
 };
 
+const add1 = () => {
+	check.textContent = '';
+	error.textContent = '';
+	buttonOne.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = '';
+	buttonOne.style.backgroundColor = 'hsl(216, 12%, 54%)';
+	check.textContent = `You selected ${buttonOneText} of ${tabLong}`;
+	console.log(allBtnRank[0].textContent);
+};
+const add2 = () => {
+	check.textContent = '';
+	buttonOne.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = 'hsl(216, 12%, 54%)';
+	check.textContent = `You selected ${buttonTwoText} of ${tabLong}`;
+	console.log(allBtnRank[1].textContent);
+};
+const add3 = () => {
+	check.textContent = '';
+	error.textContent = '';
+	buttonOne.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = 'hsl(216, 12%, 54%)';
+	check.textContent = `You selected ${buttonThreeText} of ${tabLong}`;
+	console.log(allBtnRank[2].textContent);
+};
+const add4 = () => {
+	check.textContent = '';
+	error.textContent = '';
+	buttonOne.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = 'hsl(216, 12%, 54%)';
+	check.textContent = `You selected ${buttonFourText} of ${tabLong}`;
+	console.log(allBtnRank[3].textContent);
+};
+const add5 = () => {
+	check.textContent = '';
+	error.textContent = '';
+	buttonOne.style.backgroundColor = '';
+	buttonTwo.style.backgroundColor = '';
+	buttonThree.style.backgroundColor = '';
+	buttonFour.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = '';
+	buttonFive.style.backgroundColor = 'hsl(216, 12%, 54%)';
+	check.textContent = `You selected ${buttonFiveText} of ${tabLong}`;
+	console.log(allBtnRank[4].textContent);
+};
 
-const yourNumb1 = () => {
-	
-};
-const yourNumb2 = () => {
-	console.log(twoBtn.textContent);
-};
-const yourNumb3 = () => {
-	console.log(threeBtn.textContent);
-};
-const yourNumb4 = () => {
-	console.log(fourBtn.textContent);
-};
-const yourNumb5 = () => {
-	console.log(fiveBtn.textContent);
-};
-
+buttonOne.addEventListener('click', add1);
+buttonTwo.addEventListener('click', add2);
+buttonThree.addEventListener('click', add3);
+buttonFour.addEventListener('click', add4);
+buttonFive.addEventListener('click', add5);
 mainBtn.addEventListener('click', reating);
-// btns.addEventListener('click', yourNumb1);
-// twoBtn.addEventListener('click', yourNumb2);
-// threeBtn.addEventListener('click', yourNumb3);
-// fourBtn.addEventListener('click', yourNumb4);
-// fiveBtn.addEventListener('click', yourNumb5);
